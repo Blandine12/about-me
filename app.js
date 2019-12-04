61f28cde35315062d587de18a709fa3b563d8fb4
silan <siland.dope@yahoo.fr>
2019-12-03 21:52:17 -0800

    add new files

--- app.js
+++ app.js
@@ -4,53 +4,54 @@ console.log('user\'s name was ' + username);
 
 
 var lowerCaseUsername = username.toLowerCase();
-if( lowerCaseUsername === 'blandine') {
+if (lowerCaseUsername === 'blandine') {
   alert( 'welcome back' + username);
-}else{
+} else {
   alert('welcome to my stite ' + username + '!');
 }
 
 var myname = prompt('Do you know my name?');
-console.log('user says' + myname);
+console.log('user says ' + myname);
 var lowerCasemyname = myname.toLowerCase();
-if( lowerCasemyname === 'yes') {
+if ( lowerCasemyname === 'yes') {
   alert('Great job ' + username);
-}else{
-  alert('Ask me ' + username);
+} else {
+  alert('Please, ask me ' + username);
 }
 
 var live = prompt('Do you live seattle?');
-console.log('user says');
+console.log('user says ' + live);
 var lowerCaselive = live.toLowerCase();
-if( lowerCaselive === 'yes') {
+if ( lowerCaselive === 'yes') {
   alert('seattle is beautitful');
-}else{
-  alert('You will miss life in Washington ' + username);
+} else {
+  alert('Good for you ' + username + ' , seattle is hilly');
+}
+
+var create = prompt('Can you create a website?');
+console.log('user say ' + create);
+var lowerCasecreate = live.toLowerCase();
+if ( lowerCasecreate === 'yes') {
+  alert('Great ' + username );
+} else {
+  alert('Go to code fellows, they will teach you' );
 }
 
 var site = prompt('Do want to see my site?');
-console.log('user says');
+console.log('user says '+ site);
 var lowerCasesite = site.toLowerCase();
-if( lowerCasesite === 'yes') {
+if ( lowerCasesite === 'yes') {
   alert('Thanks ' + username);
-}else{
+} else {
   alert('I will show you anyway ' + username);
 }
 
 var comment = prompt('do you want to live a comment?');
-console.log('user says');
+console.log('user says ' + comment);
 var lowerCasecomment = comment.toLowerCase();
-if( lowerCasecomment === 'yes') {
+if ( lowerCasecomment === 'yes') {
   alert('Be nice ' + username);
-}else{
-  alert('Your comment will help ' + username);
+} else {
+  alert('Your comment will help ' + username + ' ,Thanks');
 }
 
-var live = prompt('Do you live seattle?');
-console.log('user say');
-var lowerCaselive = live.toLowerCase();
-if( lowerCaselive === 'yes') {
-  alert('seattle is beautitful');
-}else{
-  alert('You will miss life in Washington ' + username);
-}
