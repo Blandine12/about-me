3c790e2eee562a584ebbfdd61fb40472eac488dd
EugeneMonnier <eugene.monnier@gmail.com>
2019-12-05 15:46:16 -0800

    add functions to questions

--- app.js
+++ app.js
@@ -12,77 +12,101 @@ if (lowerCaseUsername === 'blandine') {
   alert('welcome to my stite ' + username + '!');
 }
 
-var myname = prompt('Do you know my name?');
-console.log('user says ' + myname);
-var lowerCaseMyname = myname.toLowerCase();
-if ( lowerCaseMyname === 'yes') {
-  alert('Great job ' + username);
-} else {
-  alert('Please, ask me ' + username);
-  score ++;
-}
+questions();
 
-var live = prompt('Do you live seattle?');
-console.log('user says ' + live);
-var lowerCaselive = live.toLowerCase();
-if ( lowerCaselive === 'yes') {
-  alert('seattle is beautitful');
-} else {
-  alert('Good for you ' + username + ' , seattle is hilly');
-  score ++;
-}
-
-var create = prompt('Can you create a website?');
-console.log('user say ' + create);
-var lowerCaseCreate = live.toLowerCase();
-if ( lowerCaseCreate === 'yes') {
-  alert('Great ' + username );
-} else {
-  alert('Go to code fellows, they will teach you' );
-  score ++;
-}
-
-var site = prompt('Do want to see my site?');
-console.log('user says '+ site);
-var lowerCasesite = site.toLowerCase();
-if ( lowerCasesite === 'yes') {
-  alert('Thanks ' + username);
-} else {
-  alert('I will show you anyway ' + username);
-  score ++;
-}
-
-var comment = prompt('do you want to live a comment?');
-console.log('user says ' + comment);
-var lowerCaseComment = comment.toLowerCase();
-if ( lowerCaseComment === 'yes') {
-  alert('Be nice ' + username);
-} else {
-  alert('Your comment will help ' + username + ' ,Thanks');
-  score ++;
-}
-
-// get help Andrew
-var ageGuess = prompt('How old I am?');
-var ageAnswer = 33;
-for ( var i = 0; i < 4; i++) {
-
-  if (ageGuess < ageAnswer) {
-    ageGuess = prompt('too high');
-  } else if ( ageGuess > ageAnswer) {
-    ageAnswer = prompt('too low');
+function myNameFun() {
+  var myName = prompt('Do you know my name?');
+  console.log('user says ' + myName);
+  var lowerCaseMyname = myName.toLowerCase();
+  if ( lowerCaseMyname === 'yes' || lowerCaseMyname === 'y') {
+    alert('Great job ' + username);
   } else {
-    alert ('Correct');
+    alert('Please, ask me ' + username);
     score ++;
-    i = 4;
+  }
+}
+
+function liveFun() {
+  var live = prompt('Do you live seattle?');
+  console.log('user says ' + live);
+  var lowerCaselive = live.toLowerCase();
+  if ( lowerCaselive === 'yes' || lowerCaselive === 'y') {
+    alert('seattle is beautitful');
+  } else {
+    alert('Good for you ' + username + ' , seattle is hilly');
+    score ++;
+  }
+}
+
+function createFun() {
+  var create = prompt('Can you create a website?');
+  console.log('user say ' + create);
+  var lowerCaseCreate = create.toLowerCase();
+  if ( lowerCaseCreate === 'yes' || lowerCaseCreate === 'y') {
+    alert('Great ' + username );
+  } else {
+    alert('Go to code fellows, they will teach you' );
+    score ++;
+  }
+}
+
+function siteFun() {
+  var site = prompt('Do want to see my site?');
+  console.log('user says ' + site);
+  var lowerCasesite = site.toLowerCase();
+  if ( lowerCasesite === 'yes' || lowerCasesite === 'y') {
+    alert('Thanks ' + username);
+  } else {
+    alert('I will show you anyway ' + username);
+    score ++;
+  }
+}
+
+function commentFun() {
+  var comment = prompt('do you want to live a comment?');
+  console.log('user says ' + comment);
+  var lowerCaseComment = comment.toLowerCase();
+  if ( lowerCaseComment === 'yes' || lowerCaseComment === 'y') {
+    alert('Be nice ' + username);
+  } else {
+    alert('Your comment will help ' + username + ' ,Thanks');
+    score ++;
+  }
+}
+
+function ageGuessFun() {
+  // get help Andrew
+  var ageGuess = parseInt(prompt('How old I am?'));
+  var ageAnswer = 33;
+  for ( var i = 0; i < 4; i++) {
+    if (ageGuess < ageAnswer) {
+      ageGuess = parseInt(prompt('too low'));
+    } else if ( ageGuess > ageAnswer) {
+      ageGuess = parseInt(prompt('too high'));
+    } else if (ageGuess === ageAnswer) {
+      alert ('Correct');
+      score ++;
+      i = 4;
+    } else {
+      ageGuess = parseInt(prompt('That\'s not a number... you jackass.'));
+    }
   }
 }
 
 
+function scoreFun() {
+  alert ( ' You got ' + score + ' out of 6 questios!');
+}
 
-
-alert ( ' You got ' + score + ' out of 6 questios!');
-
+function questions() {
+  myNameFun();
+  liveFun();
+  createFun();
+  siteFun();
+  commentFun();
+  ageGuessFun();
+  scoreFun();
+}
 
 
 // var kidGuess = prompt('How many kid do I  have?');
